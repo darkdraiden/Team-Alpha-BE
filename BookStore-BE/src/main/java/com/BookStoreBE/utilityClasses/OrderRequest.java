364a3278c09fe6f1b07d5b6f52094227798e421f
sourabhch13 <sourabh.c@consultadd.com>
2024-01-25 00:50:21 +0530

    Added a utility model

---
+++ BookStore-BE/src/main/java/com/BookStoreBE/utilityClasses/OrderRequest.java
@@ -0,0 +1,20 @@
+package com.BookStoreBE.utilityClasses;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderRequest {
+    private float totalAmount;
+    private List<Integer> cartItemIds;
+    private String address;
+}
